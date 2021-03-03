f0ba3a3e16cc260415004054e2f6ca3fef1d50fc
Arnav-Ghatti <arnavghatti123@gmail.com>
2021-03-03 23:33:59 +0530

    Major changes

--- main.py
+++ main.py
@@ -7,6 +7,40 @@ root.title("Rock, Paper, Scissors")
 title = tk.Label(root, text="Rock Paper Scissors", font=("Helvetica", 15), fg="blue")
 title.grid(row=0, column=0, pady=5, padx=5)
 
+# Functions
+def check_result(user, comp):
+    # Rock > Scissor, Scissor > Paper, Paper > Rock
+
+    if user == comp:
+        result_label.config(text="Match Draw")
+    elif (user == "Rock" and comp == "Scissor") or (user == "Scissor" and comp == "Paper") or (user == "Paper" and comp == "Rock"):
+        result_label.config(text="You Win")
+    else:
+        result_label.config(text="Computer Wins")
+
+def user_choice(choice):
+    reset.config(state=tk.ENABLE)
+    
+    computer_choice = random.choice(["Rock", "Paper", "Scissor"])
+    
+    rock_btn.config(state=tk.DISABLED)
+    paper_btn.config(state=tk.DISABLED)
+    scissor_btn.config(state=tk.DISABLED)
+    
+    player_label.config(text=choice)
+    comp_label.config(text=computer_choice)
+
+    check_result(choice, computer_choice)
+
+def reset():
+    rock_btn.config(state=tk.ENABLE)
+    paper_btn.config(state=tk.ENABLE)
+    scissor_btn.config(state=tk.ENABLE)
+
+    player_label.config(text="Player")
+    comp_label.config(text="Computer")
+    result_label.config(text="")
+
 # Labels
 label_frame = tk.Frame(root)
 label_frame.grid(row=1, column=0, pady=5, padx=5)
@@ -27,13 +61,18 @@ result_label.grid(row=2, column=0, pady=5, padx=5)
 btn_frame = tk.Frame(root)
 btn_frame.grid(row=3, column=0, pady=5, padx=5)
 
-rock_btn = tk.Button(btn_frame, text="Rock", font=("Helvetica", 11))
+rock_btn = tk.Button(btn_frame, text="Rock", font=("Helvetica", 11), command=lambda: user_choice("Rock"))
 rock_btn.grid(row=0, column=0, pady=5, padx=5)
 
-paper_btn = tk.Button(btn_frame, text="Paper", font=("Helvetica", 11))
+paper_btn = tk.Button(btn_frame, text="Paper", font=("Helvetica", 11), command=lambda: user_choice("Paper"))
 paper_btn.grid(row=0, column=1, pady=5, padx=5)
 
-scissors_btn = tk.Button(btn_frame, text="Scissor", font=("Helvetica", 11))
-scissors_btn.grid(row=0, column=2, pady=5, padx=5)
+scissor_btn = tk.Button(btn_frame, text="Scissor", font=("Helvetica", 11), command=lambda: user_choice("Scissor"))
+scissor_btn.grid(row=0, column=2, pady=5, padx=5)
+
+reset = tk.Button(root, text="Reset", font=("Helvetica", 11), command=reset())
+reset.grid(row=4, column=0, pady=5, padx=5)
+
+reset.config(state=tk.DISABLED)
 
 root.mainloop()
